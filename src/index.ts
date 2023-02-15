208e9fe1236b7bbc960c96404c66a6fab80e34f2
saisunkari19 <saisunkari19@protonmail.com>
2023-02-15 13:15:52 +0530

    feat: add wallet from priv Key

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,20 @@
-import { DirectSecp256k1HdWallet } from '@cosmjs/proto-signing';
+import { DirectSecp256k1HdWallet, DirectSecp256k1Wallet } from '@cosmjs/proto-signing';
+import {fromHex} from '@cosmjs/encoding';
 
 
 (async()=>{
 
-console.log("=============================Create Wallet on Autonomy=============================");
+    console.log("=============================Create Wallet With Private Key=============================");
+
+    let privateKey = "66AF3FC220C190BF055886A13A1F7EF986C381ADEF469998D3C554C00EC7A0A2"
+    const wallet_priv = await DirectSecp256k1Wallet.fromKey(fromHex(privateKey), 'sei')
+    console.log("wallet", wallet_priv)
+
+    let [account_priv] = await wallet_priv.getAccounts()
+    console.log(account_priv.address)
+    
+
+    console.log("=============================Create Wallet on Autonomy using mnemonic=============================");
 
 // Password to Encrypt Wallet
 let password="rgukt123"
